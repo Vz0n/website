636bb02b7e03946b76bad663ab1761ebf2b81253
Vz0n <65766278+Vz0n@users.noreply.github.com>
2024-04-26 20:12:18 -0300

    Add files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portal website
+
+This is the source of the page that (was) available on https://vzondev.cf, it's nothing more that a brief bio and some of my social links.
+
+Please follow the [GPLv3](/LICENSE) license if you want to use this in your projects.
---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Functions
+async function getSocialLinks(){
+    let resp = await fetch("https://archive.vzondev.cf/api/v1/social");
+    return resp.json();
+}
+
+function setSocialLinks(){
+    let socials_el = document.getElementById("social-links");
+    getSocialLinks().then((data) => {
+       const links = data.social;
+       for(link in links){
+          let el = document.createElement("li");
+
+          // Set attributes
+          el.setAttribute("data-url", links[link]);
+          el.className = "link";
+          el.textContent = link;
+
+          // Set click event
+          el.addEventListener("click", () => {
+            window.location = el.getAttribute("data-url");
+          });
+
+          socials_el.appendChild(el);
+       }
+    }).catch((err) => {console.error("Error loading the social links: " + err)});
+
+}
+
+// Set social links dynamicly 
+setSocialLinks();
+
+// Set the footer date
+let i = document.getElementById("credits");
+let date = new Date();
+
+i.textContent = date.getFullYear() + " - Mailbox | This portal is WIP";
+
+
+
