f60fd5c36e39c8c1ad779e87cbe2c3dc5f69bc57
Vz0n <65766278+Vz0n@users.noreply.github.com>
2024-10-30 09:20:14 -0400

    set anchor's target attribute to blank, improve design & enclose js code into functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-const progammingSince = new Date(2019, 2, 9, 14, 35).getTime();
-const now = new Date().getTime();
+function getYearsProgramming(){
+
+    const progammingSince = new Date(2019, 2, 9, 14, 35).getTime();
+    const now = new Date().getTime();
+
+    // As the timestamp is in milliseconds, the time inverse factor is 1000*60*60*24*365  
+    return Math.floor((now - progammingSince)/31536000000)
+
+}
+
+// Sets the 'target' attribute of anchor elements in a tree to "blank".
+function setLinksTarget(node){
+    node.childNodes.forEach((node) => {
+        node.target = "blank"
+    });
+}
 
 let spanElement = document.getElementById("years");
-// As the timestamp is in milliseconds, the time inverse factor is 1000*60*60*24*365
-let years = (now - progammingSince)/31536000000
-
-spanElement.textContent = Math.floor(years)
+let links = document.getElementById("links");
+spanElement.textContent = getYearsProgramming();
+setLinksTarget(links);
 
